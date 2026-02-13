53c9b22859a28d6a9d4473e95b356789266265ef
tejaskopperla-code <tejas.kopperla@gmail.com>
2026-02-13 23:29:56 +0530

    Add files via upload

---
+++ Tuple_operation.py
@@ -0,0 +1,20 @@
+Tuplex = ("tuple",False,3.2,1)
+print(Tuplex)
+
+Tuplex = (4,6,2,8,3,1)
+print(Tuplex)
+
+Tuplex = Tuplex + (9,)
+print(Tuplex)
+
+Tuple1 = (50,10.60,70,50)
+print(Tuple1.count(50))
+
+Tuplex = ( 2,4,3,5,4,6,7,8,6,1)
+
+_slice = Tuplex[3:5]
+
+print(_slice)
+
+_slice = Tuplex[:6]
+print(_slice)
